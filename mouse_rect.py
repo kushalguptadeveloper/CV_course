a12edde5a30a50f7e1d3bf3f5d5db2908b32ce11
Kushal Gupta <kushalgupta@Kushals-MacBook-Pro.local>
2019-03-02 13:55:39 +0530

    drwaing rectangle through mouse

---
+++ mouse_rect.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+drawing = False
+ix,iy = -1,-1
+def draw_rectangle(event,x,y,flags,params):
+    global ix,iy,drawing
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix,iy = x,y
+        #cv2.rectangle(img,(ix,iy),(x+100,y+100),(0,255,0),-1)
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing == True:
+            cv2.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
+    elif event == cv2.EVENT_LBUTTONUP:
+        drwaing = False
+        cv2.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
+img = np.zeros((512,512,3))
+cv2.namedWindow(winname = "my_drawing")
+cv2.setMouseCallback("my_drawing",draw_rectangle)
+while True:
+    cv2.imshow("my_drawing",img)
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
+cv2.destroyAllWindows()
